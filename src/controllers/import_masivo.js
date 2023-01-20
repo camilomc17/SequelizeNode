77a2bd19716349557a867c425b5e38c0cbdbc5dd
03kristian <cristian.jacanamijoy@misena.edu.co>
2023-01-20 08:29:41 -0600

    add write file json

--- src/controllers/import_masivo.js
+++ src/controllers/import_masivo.js
@@ -7,10 +7,16 @@ function Excel () {
    const sheet = nombreHoja[0];
    const datas = XLSX.utils.sheet_to_json(excel.Sheets[sheet]);
    console.log(datas);
-  // fs.writeFile('dta.json',JSON.stringify(datas),'utf-8')
+      fs.writeFile('src/dta_excel/dta.json',JSON.stringify(datas), err => {
+       if(err){
+          console.log(err);
+         }else{
+            console.log('File succefuly written');
+        }
+  });
 };
 
 //function read()
 
 //readExcel('Atributos.xlsx');
-Excel();
+Excel(); 
